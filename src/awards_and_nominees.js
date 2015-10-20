2b9aeb5a473ce13d4b752489a38d20de68c66648
Sorensen <ruarcs@amazon.com>
2015-10-20 14:12:11 +0100

    Update with movies people won for. Remove Python file.

--- src/awards_and_nominees.js
+++ src/awards_and_nominees.js
@@ -4,120 +4,122 @@ var categories = {
 var best_actor = {
 };
 best_actor['nominees'] = [
-  'Steve Carell',
-  'Eddie Redmayne',
-  'Benedict Cumberbatch',
-  'Bradley Cooper',
-  'Michael Keaton'
+  {'actor': 'Steve Carell', 'movie': 'Foxcatcher'},
+  {'actor': 'Eddie Redmayne', 'movie': 'The Theory of Everything'},
+  {'actor': 'Benedict Cumberbatch', 'movie': 'The Imitation Game'},
+  {'actor': 'Bradley Cooper', 'movie': 'American Sniper'},
+  {'actor': 'Michael Keaton', 'movie': 'Birdman: Or (The Unexpected Virtue of Ignorance)'}
 ];
-best_actor['winner'] = 'Eddie Redmayne';
+best_actor['winner'] = {'actor': 'Eddie Redmayne', 'movie': 'The Theory of Everything'};
+best_actor['attributes'] = ['actor', 'movie'];
 categories['best_actor'] = best_actor;
 
 var best_motion_picture = {
 };
 best_motion_picture['nominees'] = [
-  'Birdman: Or (The Unexpected Virtue of Ignorance)',
-  'American Sniper',
-  'The Imitation Game',
-  'Selma',
-  'Boyhood',
-  'The Theory of Everything',
-  'Whiplash',
-  'The Grand Budapest Hotel'
+  {'movie': 'Birdman: Or (The Unexpected Virtue of Ignorance)'},
+  {'movie': 'American Sniper'},
+  {'movie': 'The Imitation Game'},
+  {'movie': 'Selma'},
+  {'movie': 'Boyhood'},
+  {'movie': 'The Theory of Everything'},
+  {'movie': 'Whiplash'},
+  {'movie': 'The Grand Budapest Hotel'}
 ];
-best_motion_picture['winner']
-= 'Birdman: Or (The Unexpected Virtue of Ignorance)';
+best_motion_picture['winner'] = {'movie': 'Birdman: Or (The Unexpected Virtue of Ignorance)'};
+best_motion_picture['attributes'] = ['movie'];
 categories['best_motion_picture'] = best_motion_picture;
 
 var best_director = {
 };
 best_director['nominees'] = [
-  'Alejandro G. Iñárritu',
-  'Richard Linklater',
-  'Bennett Miller',
-  'Wes Anderson',
-  'Morten Tyldum'
+  {'director': 'Alejandro G. Inarritu', 'movie': 'Birdman: Or (The Unexpected Virtue of Ignorance)'},
+  {'director': 'Richard Linklater', 'movie': 'Boyhood'},
+  {'director': 'Bennett Miller', 'movie': 'Foxcatcher'},
+  {'director': 'Wes Anderson', 'movie': 'The Grand Budapest Hotel'},
+  {'director': 'Morten Tyldum', 'movie': 'The Imitation Game'}
 ];
-best_director['winner']
-= 'Alejandro G. Iñárritu';
+best_director['winner'] = {'director': 'Alejandro G. Inarritu', 'movie': 'Birdman: Or (The Unexpected Virtue of Ignorance)'};
+best_director['attributes'] = ['director', 'movie'];
 categories['best_director'] = best_director;
 
 var best_actress = {
 };
 best_actress['nominees'] = [
-  'Marion Cotillard',
-  'Felicity Jones',
-  'Rosamund Pike',
-  'Reese Witherspoon',
-  'Julianne Moore'
+  {'actress': 'Marion Cotillard', 'movie': 'Two Days, One Night'},
+  {'actress': 'Felicity Jones', 'movie': 'The Theory of Everything'},
+  {'actress': 'Rosamund Pike', 'movie': 'Gone Girl'},
+  {'actress': 'Reese Witherspoon', 'movie': 'Wild'},
+  {'actress': 'Julianne Moore', 'movie': 'Still Alice'}
 ];
 best_actress['winner']
-= 'Julianne Moore';
+= {'actress': 'Julianne Moore', 'movie': 'Still Alice'};
+best_actress['attributes'] = ['actress', 'movie']
 categories['best_actress'] = best_actress;
 
 var best_animated_feature_film = {
 };
 best_animated_feature_film['nominees'] = [
-  'The Boxtrolls',
-  'How to Train Your Dragon 2',
-  'Big Hero 6',
-  'Song of the Sea',
-  'The Tale of the Princess Kaguya'
+  {'movie': 'The Boxtrolls'},
+  {'movie': 'How to Train Your Dragon 2'},
+  {'movie': 'Big Hero 6'},
+  {'movie': 'Song of the Sea'},
+  {'movie': 'The Tale of the Princess Kaguya'}
 ];
-best_animated_feature_film['winner']
-= 'Big Hero 6';
+best_animated_feature_film['winner'] = {'movie': 'Big Hero 6'};
+best_animated_feature_film['attributes'] = ['movie']
 categories['best_animated_feature_film'] = best_animated_feature_film;
 
 var best_achievement_in_visual_effects = {
 };
 best_achievement_in_visual_effects['nominees'] = [
-  'Captain America: The Winter Soldier',
-  'Interstellar',
-  'Dawn of the Planet of the Apes',
-  'Guardians of the Galaxy',
-  'X-Men: Days of Future Past'
+  {'movie': 'Captain America: The Winter Soldier'},
+  {'movie': 'Interstellar'},
+  {'movie': 'Dawn of the Planet of the Apes'},
+  {'movie': 'Guardians of the Galaxy'},
+  {'movie': 'X-Men: Days of Future Past'}
 ];
-best_achievement_in_visual_effects['winner']
-= 'Big Hero 6';
+best_achievement_in_visual_effects['winner'] = {'movie': 'Interstellar'};
+best_achievement_in_visual_effects['attributes'] = ['movie'];
 categories['best_achievement_in_visual_effects'] = best_achievement_in_visual_effects;
 
 var best_supporting_actress = {
 };
 best_supporting_actress['nominees'] = [
-  'Laura Dern',
-  'Keira Knightley',
-  'Emma Stone',
-  'Patricia Arquette',
-  'Meryl Streep'
+  {'actress': 'Laura Dern', 'movie': 'Wild'},
+  {'actress': 'Keira Knightley', 'movie': 'The Imitation Game'},
+  {'actress': 'Emma Stone', 'movie': 'Birdman: Or (The Unexpected Virtue of Ignorance)'},
+  {'actress': 'Patricia Arquette', 'movie': 'Boyhood'},
+  {'actrees': 'Meryl Streep', 'movie': 'Into the Woods'}
 ];
-best_supporting_actress['winner']
-= 'Patricia Arquette';
+best_supporting_actress['winner'] = {'actress': 'Patricia Arquette', 'movie': 'Boyhood'};
+best_supporting_actress['attributes'] = ['actress', 'movie'];
 categories['best_supporting_actress'] = best_supporting_actress;
 
 var best_foreign_language_film = {
 };
 best_foreign_language_film['nominees'] = [
-  'Tangerines',
-  'Leviathan',
-  'Ida',
-  'Wild Tales',
-  'Timbuktu'
+  {'movie': 'Tangerines'},
+  {'movie': 'Leviathan'},
+  {'movie': 'Ida'},
+  {'movie': 'Wild Tales'},
+  {'movie': 'Timbuktu'}
 ];
-best_foreign_language_film['winner']
-= 'Ida';
+best_foreign_language_film['winner'] = {'movie': 'Ida'};
+best_foreign_language_film['attributes'] = ['movie'];
 categories['best_foreign_language_film'] = best_foreign_language_film;
 
 var best_supporting_actor = {
 };
 best_supporting_actor['nominees'] = [
-  'Robert Duvall',
-  'JK Simmons',
-  'Ethan Hawke',
-  'Edward Norton',
-  'Mark Ruffalo'
+  {'actor': 'Robert Duvall', 'movie': 'The Judge'},
+  {'actor': 'JK Simmons', 'movie': 'Whiplash'},
+  {'actor': 'Ethan Hawke', 'movie': 'Boyhood'},
+  {'actor': 'Edward Norton', 'movie': 'Birdman: Or (The Unexpected Virtue of Ignorance)'},
+  {'actor': 'Mark Ruffalo', 'movie': 'Foxcatcher'},
 ];
-best_supporting_actor['winner']
-= 'JK Simmons';
+best_supporting_actor['winner'] = {'actor': 'JK Simmons', 'movie': 'Whiplash'};
+best_supporting_actor['attributes'] = ['actor', 'movie'];
 categories['best_supporting_actor'] = best_supporting_actor;
 
 module.exports = categories;
